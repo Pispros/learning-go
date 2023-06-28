798d587f9344d1df5f8475010ee225c989a80a0f
Pispros <dl@sherpa-solutions.com>
2023-06-28 02:02:16 -0400

    Create request from csv and insert into sql server Azure

--- etl-mti-820.go
+++ etl-mti-820.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/microsoft/go-mssqldb"
 )
+
+var db *sql.DB
+
+var server = "mit820dbserver.database.windows.net"
+
+var port = 1433
+
+var user = "CloudSAb454d64f"
+
+var password = "MTI820PasswordBixi"
+
+var database = "mti820Database"
 
 func main() {
 
-	if len(os.Args) >= 5 {
+	if len(os.Args) >= 4 {
 
-		var tableType string = os.Args[1]
+		var tableToInsert string = os.Args[1]
 
-		var tableToInsert string = os.Args[2]
+		var databaseToInsert string = os.Args[2]
 
-		var databaseToInsert string = os.Args[3]
+		var filePath string = os.Args[3]
 
-		var filePath string = os.Args[4]
+		// Build connection string
+
+		connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+
+			server, user, password, port, databaseToInsert)
+
+		var err error
+
+		// Create connection pool
+
+		db, err = sql.Open("sqlserver", connString)
+
+		if err != nil {
+
+			log.Fatal("Error creating connection pool: ", err.Error())
+
+		}
+
+		ctx := context.Background()
+
+		err = db.PingContext(ctx)
+
+		if err != nil {
+
+			log.Fatal(err.Error())
+
+		}
+
+		fmt.Printf("Connected!")
 
 		fmt.Println(" ----------- Starting extraction and insertion")
 
@@ -40,58 +83,54 @@ func main() {
 			fmt.Println("Error reading records")
 		}
 
-		var connectionString string = "go:Passer1234@tcp(127.0.0.1:3306)/" + databaseToInsert
+		//Connexion to Local database
+		// var connectionString string = "go:Passer1234@tcp(127.0.0.1:3306)/" + databaseToInsert
 
-		db, err := sql.Open("mysql", connectionString)
+		// db, err := sql.Open("mysql", connectionString)
 
-		if err != nil {
-			panic(error.Error(err))
+		// if err != nil {
+		// 	panic(error.Error(err))
+		// }
+
+		// defer db.Close()
+
+		var tableStructString string = "("
+
+		for i := 0; i < len(records[0]); i++ {
+			tableStructString += records[0][i] + ","
 		}
+		tableStructString = tableStructString[0:len(tableStructString)-1] + ")"
+		fmt.Println(tableStructString)
 
-		defer db.Close()
-
-		// Shift first line
 		records = records[1:len(records)]
 
-		if tableType == "station" {
-			for _, eachrecord := range records {
-				var insertString string = "INSERT INTO " + tableToInsert + " VALUE(\"" + eachrecord[0] + "\",\"" + eachrecord[1] + "\",\"" + eachrecord[2] + "\",\"" + eachrecord[3] + "\");"
-				fmt.Println(insertString)
-				insert, err := db.Query(insertString)
-
-				if err != nil {
-					panic(error.Error(err))
-				}
-
-				insert.Close()
+		for _, eachrecord := range records {
+			var dataToInsert string = ""
+			for i := 0; i < len(eachrecord); i++ {
+				eachrecord[i] = strings.ReplaceAll(eachrecord[i], "'", "")
+				dataToInsert += "'" + eachrecord[i] + "',"
 			}
-		}
+			dataToInsert = dataToInsert[0 : len(dataToInsert)-1]
+			var insertString string = "INSERT INTO " + tableToInsert + tableStructString + " VALUES(" + dataToInsert + ");"
+			fmt.Println(insertString)
+			insert, err := db.Query(insertString)
 
-		if tableType == "deplacement" {
-			for _, eachrecord := range records {
-				var insertString string = "INSERT INTO " + tableToInsert + " VALUE(\"" + eachrecord[0] + "\",\"" + eachrecord[1] + "\",\"" + eachrecord[2] + "\",\"" + eachrecord[3] + "\",\"" + eachrecord[4] + "\");"
-				fmt.Println(insertString)
-				insert, err := db.Query(insertString)
-
-				if err != nil {
-					panic(error.Error(err))
-				}
-
-				insert.Close()
+			if err != nil {
+				panic(error.Error(err))
 			}
-		}
 
+			insert.Close()
+		}
 		fmt.Println(" -----------All Data inserted successfully")
 	} else {
 		fmt.Println("")
-		fmt.Println("Veuillez fournir une base de données et une source dans les paramètre")
+		fmt.Println("Veuillez fournir une base de données et une source dans les paramètres")
 		fmt.Println("")
-		fmt.Println("----------- > go run ex.go parametre1 parametre2 parametre3 parametre4")
+		fmt.Println("----------- > go run ex.go parametre1 parametre2 parametre3")
 		fmt.Println("")
-		fmt.Println("parametre 1 = type de table")
-		fmt.Println("parametre 2 = table de destination")
-		fmt.Println("parametre 3 = Base de donnée")
-		fmt.Println("parametre 4 = fichier csv source")
+		fmt.Println("parametre 1 = table de destination")
+		fmt.Println("parametre 2 = Base de donnée")
+		fmt.Println("parametre 3 = fichier csv source")
 		fmt.Println("-")
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module etl
 go 1.20
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require (
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/microsoft/go-mssqldb v1.1.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+)
